75aa23409cee644a7d7076676b3dd28595648b61
Alexander Elfimow <my.free.mail@gmx.de>
2020-11-28 21:35:37 +0100

    cas_number: added functions

--- src/cas_number.cpp
+++ src/cas_number.cpp
@@ -1,10 +1,29 @@
 #include "cas_number.h"
 
 
-cas_number::cas_number()
+cas_number::cas_number(std::string const &str) :
+    m_cas_number_str { str },
+    m_formula { },
+    m_synonym { }
 {
 }
 
 cas_number::~cas_number()
+{
+}
+
+cas_number &cas_number::formula(std::string const &str)
+{
+    m_formula = str;
+    return *this;
+}
+
+cas_number &cas_number::synonym(std::string const &str)
+{
+    m_synonym = str;
+    return *this;
+}
+
+void cas_number::check()
 {
 }
--- src/cas_number.h
+++ src/cas_number.h
@@ -3,11 +3,26 @@
 #else
 #define CAS_NUMBER_H
 
+#include <string>
+
 class cas_number
 {
     public:
-        cas_number();
+        explicit cas_number(std::string const &str);
         ~cas_number();
+
+        cas_number &formula(std::string const &str);
+        cas_number &synonym(std::string const &str);
+
+        void check();
+
+    private:
+        std::string m_cas_number_str;
+        std::string m_formula;
+        std::string m_synonym;
+
+    public:
+        cas_number() = delete;
 };
 
 #endif
