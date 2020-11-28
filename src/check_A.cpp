48a657d893546b82a3a9bd4b7de60430cc22f660
Alexander Elfimow <my.free.mail@gmx.de>
2020-11-28 22:04:54 +0100

    Added files

---
+++ src/check_A.cpp
@@ -0,0 +1,590 @@
+#include "check_A.h"
+
+void check_A()
+{
+    {
+        cas_number casnr { "12002-61-8" };
+        casnr.formula("Ac2O3").synonym("actinium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "27039-77-6" };
+        casnr.formula("AgAlCl4").synonym("silver tetrachloroaluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7785-23-1" };
+        casnr.formula("AgBr").synonym("silver bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-89-3" };
+        casnr.formula("AgBrO3").synonym("silver bromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "506-64-9" };
+        casnr.formula("AgCN").synonym("silver cyanide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "563-63-3" };
+        casnr.formula("AgC2H3O2").synonym("silver acetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-90-6" };
+        casnr.formula("AgCl").synonym("silver chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-92-8" };
+        casnr.formula("AgClO3").synonym("silver chlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-93-9" };
+        casnr.formula("AgClO4").synonym("silver perchlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7775-41-9" };
+        casnr.formula("AgF").synonym("silver fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-95-1" };
+        casnr.formula("AgF2").synonym("silver difluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-96-2" };
+        casnr.formula("AgI").synonym("silver iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-97-3" };
+        casnr.formula("AgIO3").synonym("silver iodate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-98-4" };
+        casnr.formula("AgMnO4").synonym("silver permanganate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13863-88-2" };
+        casnr.formula("AgN3").synonym("silver azide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7761-88-8" };
+        casnr.formula("AgNO3").synonym("silver nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1301-96-8" };
+        casnr.formula("AgO").synonym("silver(I,III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "5610-59-3" };
+        casnr.formula("AgONC").synonym("silver fulminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "26042-63-7" };
+        casnr.formula("ArPF6").synonym("silver hexafluorophosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-00-1" };
+        casnr.formula("AgReO4").synonym("silver perrhenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1701-93-5" };
+        casnr.formula("AgSNC").synonym("silver thiocyanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7659-31-6" };
+        casnr.formula("Ag2C2").synonym("silver acetylide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "534-16-7" };
+        casnr.formula("Ag2CO3").synonym("silver carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "533-51-7" };
+        casnr.formula("Ag2C2O4").synonym("silver oxalate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "75763-82-5" };
+        casnr.formula("Ag2Cl2").synonym("disilver dichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-01-2" };
+        casnr.formula("Ag2CrO4").synonym("silver chromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-02-3" };
+        casnr.formula("Ag2Cr2O7").synonym("silver dichromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1302-01-8" };
+        casnr.formula("Ag2F").synonym("silver subfluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13765-74-7" };
+        casnr.formula("Ag2MoO4").synonym("silver molybdate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20667-12-3" };
+        casnr.formula("Ag2O").synonym("silver oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "21548-73-2" };
+        casnr.formula("Ag2S").synonym("silver sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-26-5" };
+        casnr.formula("Ag2SO4").synonym("silver sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1302-09-6" };
+        casnr.formula("Ag2Se").synonym("silver selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-05-6" };
+        casnr.formula("Ag2SeO3").synonym("silver selenite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-07-8" };
+        casnr.formula("Ag2SeO4").synonym("silver selenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12002-99-2" };
+        casnr.formula("Ag2Te").synonym("silver telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "11078-32-3" };
+        casnr.formula("Ag3Br2").synonym("silver dibromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "11078-33-4" };
+        casnr.formula("Ag3Br3").synonym("silver tribromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12444-96-1" };
+        casnr.formula("Ag3Cl3").synonym("trisilver trichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "37375-12-5" };
+        casnr.formula("Ag3I3").synonym("trisilver triiodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-09-0" };
+        casnr.formula("Ag3PO4").synonym("silver phosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15983-65-0" };
+        casnr.formula("Ag3SbS3").synonym("silver thioantimonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7429-90-5" };
+        casnr.formula("Al").synonym("aluminium");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "22831-42-1" };
+        casnr.formula("AlAs").synonym("aluminium arsenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12041-50-8" };
+        casnr.formula("AlB2").synonym("aluminium diboride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12041-54-2" };
+        casnr.formula("AlB12").synonym("aluminium dodecaboride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7727-15-3" };
+        casnr.formula("AlBr3").synonym("aluminium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-11-4" };
+        casnr.formula("AlBr3•6H2O").synonym("aluminium bromide hexahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7360-53-4" };
+        casnr.formula("Al(CHO2)3").synonym("aluminium formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-70-0" };
+        casnr.formula("AlCl3").synonym("aluminium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-13-6" };
+        casnr.formula("AlCl3•6H2O").synonym("aluminium chloride hexahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-18-1" };
+        casnr.formula("AlF3").synonym("aluminium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-23-8" };
+        casnr.formula("AlI3").synonym("aluminium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "24304-00-5" };
+        casnr.formula("AlN").synonym("aluminium nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13473-90-0" };
+        casnr.formula("Al(NO3)3").synonym("aluminium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "21645-51-2" };
+        casnr.formula("Al(OH)3").synonym("aluminium hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20859-73-8" };
+        casnr.formula("AlP").synonym("aluminium phosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-30-7" };
+        casnr.formula("AlPO4").synonym("aluminium phosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25152-52-7" };
+        casnr.formula("AlSb").synonym("aluminium antimonide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1344-28-1" };
+        casnr.formula("Al2O3").synonym("aluminium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10043-01-3" };
+        casnr.formula("Al2(SO4)3").synonym("aluminium sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1302-81-4" };
+        casnr.formula("Al2S3").synonym("aluminium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1302-82-5" };
+        casnr.formula("Al2Se3").synonym("aluminium selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1299-86-1" };
+        casnr.formula("Al4C3").synonym("aluminum carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1302-93-8" };
+        casnr.formula("Al6O13Si2").synonym("aluminum silicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13464-46-5" };
+        casnr.formula("AmCl3").synonym("americium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12005-67-3" };
+        casnr.formula("AmO2").synonym("americium dioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7440-38-2" };
+        casnr.formula("As").synonym("arsenic");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "82868-10-8" };
+        casnr.formula("AsBrO").synonym("arsenic oxybromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-33-0" };
+        casnr.formula("AsBr3").synonym("arsenic tribromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-34-1" };
+        casnr.formula("AsCl3").synonym("arsenic trichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "60646-36-8" };
+        casnr.formula("AsCl3O").synonym("arsenic oxychloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "22441-45-8" };
+        casnr.formula("AsCl5").synonym("arsenic pentachloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-35-2" };
+        casnr.formula("AsF3").synonym("arsenic trifluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-36-3" };
+        casnr.formula("AsF5").synonym("arsenic pentafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-42-1" };
+        casnr.formula("AsH3").synonym("arsenic trihydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7784-45-4" };
+        casnr.formula("AsI3").synonym("arsenic triiodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12005-99-1" };
+        casnr.formula("AsO").synonym("arsenic monoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12255-12-8" };
+        casnr.formula("AsO2").synonym("arsenic dioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12255-33-3" };
+        casnr.formula("AsP").synonym("arsenic monophosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12511-95-4" };
+        casnr.formula("AsP3").synonym("arsenic phosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12006-06-3" };
+        casnr.formula("AsSe4").synonym("arsenic tetraselenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15942-63-9" };
+        casnr.formula("As2H4").synonym("diarsenic tetrahydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13770-56-4" };
+        casnr.formula("As2I4").synonym("arsenic diiodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1327-53-3" };
+        casnr.formula("As2O3").synonym("arsenic trioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-28-2" };
+        casnr.formula("As2O5").synonym("arsenic pentoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12512-03-7" };
+        casnr.formula("As2P2").synonym("arsenic diphosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-33-9" };
+        casnr.formula("As2S3").synonym("arsenic trisulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-32-8" };
+        casnr.formula("As2S4").synonym("arsenic tetrasulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-34-0" };
+        casnr.formula("As2S5").synonym("arsenic pentasulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-35-1" };
+        casnr.formula("As2Se").synonym("arsenic hemiselenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-36-2" };
+        casnr.formula("As2Se3").synonym("arsenic triselenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-37-3" };
+        casnr.formula("As2Se5").synonym("arsenic pentaselenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12044-54-1" };
+        casnr.formula("As2Te3").synonym("arsenic tritelluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "83527-53-1" };
+        casnr.formula("As3O4").synonym("arsenic tetraoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12512-11-7" };
+        casnr.formula("As3P").synonym("arsenic triphosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12279-90-2" };
+        casnr.formula("As4S4").synonym("tetraarsenic tetrasulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7440-57-5" };
+        casnr.formula("Au").synonym("gold");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-27-6" };
+        casnr.formula("AuBr").synonym("gold(I) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-28-7" };
+        casnr.formula("AuBr3").synonym("gold(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "506-65-0" };
+        casnr.formula("AuCN").synonym("gold(I) cyanide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-29-8" };
+        casnr.formula("AuCl").synonym("gold(I) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13453-07-1" };
+        casnr.formula("AuCl3").synonym("gold(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14720-21-9" };
+        casnr.formula("AuF3").synonym("gold(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "57542-85-5" };
+        casnr.formula("AuF5").synonym("gold(V) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-31-2" };
+        casnr.formula("AuI").synonym("gold(I) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "31032-13-0" };
+        casnr.formula("AuI3").synonym("gold(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-52-2" };
+        casnr.formula("Au(OH)3").synonym("gold(III) hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "23331-11-5" };
+        casnr.formula("AuSe").synonym("gold(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "37043-71-3" };
+        casnr.formula("AuTe").synonym("gold(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-58-8" };
+        casnr.formula("Au2O3").synonym("gold(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-60-2" };
+        casnr.formula("Au2S").synonym("gold(I) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-61-3" };
+        casnr.formula("Au2S3").synonym("gold(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-32-3" };
+        casnr.formula("Au2(SeO4)3").synonym("gold(III) selenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1303-62-4" };
+        casnr.formula("Au2Se3").synonym("gold(III) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "62792-24-9" };
+        casnr.formula("Au4Cl8").synonym("gold(I,III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "" };
+        casnr.formula("Au4F8").synonym("gold(I,III) fluoride");
+        casnr.check();
+    }
+}
---
+++ src/check_A.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_A_H
+#error Already included
+#else
+#define CHECK_A_H
+
+extern void check_A();
+
+#endif
