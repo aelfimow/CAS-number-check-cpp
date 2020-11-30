0b76aff6e2a7e0bfc77e363ccffcfca92137958d
Alexander Elfimow <my.free.mail@gmx.de>
2020-11-30 19:42:08 +0100

    Added files

---
+++ src/check_H.cpp
@@ -0,0 +1,251 @@
+#include "check_H.h"
+#include "cas_number.h"
+
+void check_H()
+{
+    {
+        cas_number casnr { "7732-18-5" };
+        casnr.formula("H2O").synonym("water");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7664-93-9" };
+        casnr.formula("H2SO4").synonym("sulfuric acid");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10043-35-3" };
+        casnr.formula("H3BO3").synonym("boric acid");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12007-23-7" };
+        casnr.formula("HfB2").synonym("hafnium boride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13777-22-5" };
+        casnr.formula("HfBr4").synonym("hafnium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12069-85-1" };
+        casnr.formula("HfC").synonym("hafnium carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13499-05-3" };
+        casnr.formula("HfCl4").synonym("hafnium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13709-52-9" };
+        casnr.formula("HfF4").synonym("hafnium(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12770-26-2" };
+        casnr.formula("HfH2").synonym("hafnium hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13779-73-2" };
+        casnr.formula("HfI3").synonym("hafnium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25817-87-2" };
+        casnr.formula("HfN").synonym("hafnium nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12055-23-1" };
+        casnr.formula("HfO2").synonym("hafnium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12325-59-6" };
+        casnr.formula("HfP").synonym("hafnium phosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15823-43-5" };
+        casnr.formula("Hf(SO4)2").synonym("hafnium sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18855-94-2" };
+        casnr.formula("HfS2").synonym("hafnium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12162-21-9" };
+        casnr.formula("HfSe2").synonym("hafnium selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13870-13-8" };
+        casnr.formula("HfSiO4").synonym("hafnium orthosilicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12401-56-8" };
+        casnr.formula("HfSi2").synonym("hafnium silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-47-1" };
+        casnr.formula("HgBr2").synonym("mercury(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1600-27-7" };
+        casnr.formula("Hg(C2H3O2)2").synonym("mercury(II) acetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7487-94-7" };
+        casnr.formula("HgCl2").synonym("mercury(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "73491-34-6" };
+        casnr.formula("HgCl2O8").synonym("mercury(II) perchlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-39-3" };
+        casnr.formula("HgF2").synonym("mercury(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "72172-67-9" };
+        casnr.formula("HgH2").synonym("mercury(II) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-32-6" };
+        casnr.formula("Hg(IO3)2").synonym("mercury(II) iodate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7774-29-0" };
+        casnr.formula("HgI2").synonym("mercury(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10045-94-0" };
+        casnr.formula("Hg(NO3)2").synonym("mercury(II) nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "21908-53-2" };
+        casnr.formula("HgO").synonym("mercury(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "628-86-4" };
+        casnr.formula("Hg(ONC)2").synonym("mercury(II) fulminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1344-48-5" };
+        casnr.formula("HgS").synonym("mercury(II) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "592-85-8" };
+        casnr.formula("Hg(SCN)2").synonym("mercury(II) thiocyanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-35-9" };
+        casnr.formula("HgSO4").synonym("mercury(II) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20601-83-6" };
+        casnr.formula("HgSe").synonym("mercury(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12068-90-5" };
+        casnr.formula("HgTe").synonym("mercury(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15385-58-7" };
+        casnr.formula("Hg2Br2").synonym("mercury(I) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10112-91-1" };
+        casnr.formula("Hg2Cl2").synonym("mercury(I) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1396-75-4" };
+        casnr.formula("Hg2F2").synonym("mercury(I) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15385-57-6" };
+        casnr.formula("Hg2I2").synonym("mercury(I) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15829-53-5" };
+        casnr.formula("Hg2O").synonym("mercury(I) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13825-76-8" };
+        casnr.formula("HoBr3").synonym("holmium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "3252-59-3" };
+        casnr.formula("Ho(CHO2)3").synonym("holmium formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10138-62-2" };
+        casnr.formula("HoCl3").synonym("holmium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13760-78-6" };
+        casnr.formula("HoF3").synonym("holmium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13813-41-7" };
+        casnr.formula("HoI3").synonym("holmium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12029-81-1" };
+        casnr.formula("HoN").synonym("holmium nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12136-24-2" };
+        casnr.formula("HoSi2").synonym("holmium silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13977-63-4" };
+        casnr.formula("HoVO4").synonym("holmium vanadate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12055-62-8" };
+        casnr.formula("Ho2O3").synonym("holmium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12162-59-3" };
+        casnr.formula("Ho2S3").synonym("holmium sulfide");
+        casnr.check();
+    }
+}
---
+++ src/check_H.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_H_H
+#error Already included
+#else
+#define CHECK_H_H
+
+extern void check_H();
+
+#endif
