1f56939b9b9bf13e3ce4f4840bb8557cc510f000
Alexander Elfimow <my.free.mail@gmx.de>
2020-11-30 19:37:44 +0100

    Added files

---
+++ src/check_G.cpp
@@ -0,0 +1,281 @@
+#include "check_G.h"
+#include "cas_number.h"
+
+void check_G()
+{
+    {
+        cas_number casnr { "1303-00-0" };
+        casnr.formula("GaAs").synonym("gallium(III) arsenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13450-88-9" };
+        casnr.formula("GaBr3").synonym("gallium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "24597-12-4" };
+        casnr.formula("GaCl2").synonym("gallium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13450-90-3" };
+        casnr.formula("GaCl3").synonym("gallium trichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-51-9" };
+        casnr.formula("GaF3").synonym("gallium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "22886-66-4" };
+        casnr.formula("GaF3•3H2O").synonym("gallium(III) fluoride trihydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13572-93-5" };
+        casnr.formula("GaH3").synonym("gallium(III) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13450-91-4" };
+        casnr.formula("GaI3").synonym("gallium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25617-97-4" };
+        casnr.formula("GaN").synonym("gallium(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13494-90-1" };
+        casnr.formula("Ga(NO3)3").synonym("gallium(III) nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-08-7" };
+        casnr.formula("GaO").synonym("gallium(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12023-99-3" };
+        casnr.formula("Ga(OH)3").synonym("gallium(III) hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12063-98-8" };
+        casnr.formula("GaP").synonym("gallium(III) phosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-10-1" };
+        casnr.formula("GaS").synonym("gallium(II) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12064-03-8" };
+        casnr.formula("GaSb").synonym("gallium(III) antimonide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-11-2" };
+        casnr.formula("GaSe").synonym("gallium(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-14-5" };
+        casnr.formula("GaTe").synonym("gallium(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "24597-12-4" };
+        casnr.formula("Ga2Cl4").synonym("gallium(I,III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-20-3" };
+        casnr.formula("Ga2O").synonym("gallium(I) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-21-4" };
+        casnr.formula("Ga2O3").synonym("gallium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-22-5" };
+        casnr.formula("Ga2S3").synonym("gallium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13494-91-2" };
+        casnr.formula("Ga2(SO4)3").synonym("gallium(III) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-24-7" };
+        casnr.formula("Ga2Se3").synonym("gallium(III) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-27-0" };
+        casnr.formula("Ga2Te3").synonym("gallium(III) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13818-75-2" };
+        casnr.formula("GdBr3").synonym("gadolinium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "3252-56-0" };
+        casnr.formula("Gd(CHO2)3").synonym("gadolinium(III) formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10138-52-0" };
+        casnr.formula("GdCl3").synonym("gadolinium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "19423-81-5" };
+        casnr.formula("GdCl3•6H2O").synonym("gadolinium(III) chloride hexahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13765-26-9" };
+        casnr.formula("GdF3").synonym("gadolinium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13814-72-7" };
+        casnr.formula("GdI2").synonym("gadolinium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13572-98-0" };
+        casnr.formula("GdI3").synonym("gadolinium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25764-15-2" };
+        casnr.formula("GdN").synonym("gadolinium(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12024-81-6" };
+        casnr.formula("GdSe").synonym("gadolinium(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12134-75-7" };
+        casnr.formula("GdSi2").synonym("gadolinium(II) silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12064-62-9" };
+        casnr.formula("Gd2O3").synonym("gadolinium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12134-77-9" };
+        casnr.formula("Gd2S3").synonym("gadolinium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12160-99-5" };
+        casnr.formula("Gd2Te3").synonym("gadolinium(III) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "24415-00-7" };
+        casnr.formula("GeBr2").synonym("germanium(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13450-92-5" };
+        casnr.formula("GeBr4").synonym("germanium(IV) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "992-91-6" };
+        casnr.formula("Ge(CH3O)4").synonym("germanium(IV) methoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10060-11-4" };
+        casnr.formula("GeCl2").synonym("germanium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10038-98-9" };
+        casnr.formula("GeCl4").synonym("germanium(IV) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13940-63-1" };
+        casnr.formula("GeF2").synonym("germanium(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-58-6" };
+        casnr.formula("GeF4").synonym("germanium(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7782-65-2" };
+        casnr.formula("GeH4").synonym("germanium(IV) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13573-08-5" };
+        casnr.formula("GeI2").synonym("germanium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13450-95-8" };
+        casnr.formula("GeI4").synonym("germanium(IV) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20619-16-3" };
+        casnr.formula("GeO").synonym("germanium(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1310-53-8" };
+        casnr.formula("GeO2").synonym("germanium(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12025-32-0" };
+        casnr.formula("GeS").synonym("germanium(II) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12025-34-2" };
+        casnr.formula("GeS2").synonym("germanium(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12065-10-0" };
+        casnr.formula("GeSe").synonym("germanium(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12065-11-1" };
+        casnr.formula("GeSe2").synonym("germanium(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12025-39-7" };
+        casnr.formula("GeTe").synonym("germanium(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12065-36-0" };
+        casnr.formula("Ge3N4").synonym("germanium(IV) nitride");
+        casnr.check();
+    }
+}
---
+++ src/check_G.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_G_H
+#error Already included
+#else
+#define CHECK_G_H
+
+extern void check_G();
+
+#endif
