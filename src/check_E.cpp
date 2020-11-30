2764feee343f8ab076624300590ee06b7407d2e5
Alexander Elfimow <my.free.mail@gmx.de>
2020-11-30 19:28:09 +0100

    Added files

---
+++ src/check_E.cpp
@@ -0,0 +1,221 @@
+#include "check_E.h"
+#include "cas_number.h"
+
+void check_E()
+{
+    {
+        cas_number casnr { "12310-44-0" };
+        casnr.formula("ErB4").synonym("erbium(III) boride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13536-73-7" };
+        casnr.formula("ErBr3").synonym("erbium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15331-72-3" };
+        casnr.formula("Er(CHO2)3").synonym("erbium(III) formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10138-41-7" };
+        casnr.formula("ErCl3").synonym("erbium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-75-9" };
+        casnr.formula("ErCl3•6H2O").synonym("erbium(III) chloride hexahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13760-83-3" };
+        casnr.formula("ErF3").synonym("erbium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13550-53-3" };
+        casnr.formula("ErH3").synonym("erbium(III) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13813-42-8" };
+        casnr.formula("ErI3").synonym("erbium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12020-21-2" };
+        casnr.formula("ErN").synonym("erbium(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12061-00-6" };
+        casnr.formula("ErNbO4").synonym("erbium(III) niobate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12159-66-9" };
+        casnr.formula("ErS").synonym("erbium(II) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12020-39-2" };
+        casnr.formula("ErTe").synonym("erbium(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13596-17-3" };
+        casnr.formula("ErVO4").synonym("erbium(III) vanadate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12061-16-4" };
+        casnr.formula("Er2O3").synonym("erbium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13478-49-4" };
+        casnr.formula("Er2(SO4)3").synonym("erbium(III) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12159-66-9" };
+        casnr.formula("Er2S3").synonym("erbium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12020-39-2" };
+        casnr.formula("Er2Te3").synonym("erbium(III) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "70292-43-2" };
+        casnr.formula("EsBr2").synonym("einsteinium(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "72461-17-7" };
+        casnr.formula("EsBr3").synonym("einsteinium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "66693-95-6" };
+        casnr.formula("EsCl2").synonym("einsteinium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "24645-86-1" };
+        casnr.formula("EsCl3").synonym("einsteinium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "99644-27-6" };
+        casnr.formula("EsF3").synonym("einsteinium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "70292-44-3" };
+        casnr.formula("EsI2").synonym("einsteinium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "99644-28-7" };
+        casnr.formula("EsI3").synonym("einsteinium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "37362-94-0" };
+        casnr.formula("Es2O3").synonym("einsteinium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13780-48-8" };
+        casnr.formula("EuBr2").synonym("europium(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13759-88-1" };
+        casnr.formula("EuBr3").synonym("europium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "3252-55-9" };
+        casnr.formula("Eu(CHO2)3").synonym("europium(III) formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13769-20-5" };
+        casnr.formula("EuCl2").synonym("europium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-76-0" };
+        casnr.formula("EuCl3").synonym("europium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14077-39-5" };
+        casnr.formula("EuF2").synonym("europium(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13765-25-8" };
+        casnr.formula("EuF3").synonym("europium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "22015-35-6" };
+        casnr.formula("EuI2").synonym("europium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13759-90-5" };
+        casnr.formula("EuI3").synonym("europium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12020-58-5" };
+        casnr.formula("EuN").synonym("europium(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "55216-32-5" };
+        casnr.formula("Eu(NbO3)2").synonym("europium(II) niobate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12020-65-4" };
+        casnr.formula("EuS").synonym("europium(II) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12020-66-5" };
+        casnr.formula("EuSe").synonym("europium(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12434-24-1" };
+        casnr.formula("EuSi2").synonym("europium(II) silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12020-69-8" };
+        casnr.formula("EuTe").synonym("europium(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13537-11-6" };
+        casnr.formula("EuVO4").synonym("europium(II) vanadate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1308-96-9" };
+        casnr.formula("Eu2O3").synonym("europium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13537-15-0" };
+        casnr.formula("Eu2(SO4)3").synonym("europium(III) sulfate");
+        casnr.check();
+    }
+}
---
+++ src/check_E.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_E_H
+#error Already included
+#else
+#define CHECK_E_H
+
+extern void check_E();
+
+#endif
