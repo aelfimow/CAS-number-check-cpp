8c246afc8f7b82ad53b3915d2dcfed0bb6e15a3e
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-03 21:37:43 +0100

    Added files

---
+++ src/check_L.cpp
@@ -0,0 +1,376 @@
+#include "check_L.h"
+#include "cas_number.h"
+
+void check_L()
+{
+    {
+        cas_number casnr { "71496-78-1" };
+        casnr.formula("LaAlO3").synonym("lanthanum aluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12008-21-8" };
+        casnr.formula("LaB6").synonym("lanthanum boride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13536-79-3" };
+        casnr.formula("LaBr3").synonym("lanthanum bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "2081-11-0" };
+        casnr.formula("La(CHO2)3").synonym("lanthanum formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12071-15-7" };
+        casnr.formula("LaC2").synonym("lanthanum carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10099-58-8" };
+        casnr.formula("LaCl3").synonym("lanthanum chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "35564-84-2" };
+        casnr.formula("LaCl3•3H2O").synonym("lanthanum chloride trihydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-84-0" };
+        casnr.formula("LaCl3•7H2O").synonym("lanthanum chloride heptahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13709-38-1" };
+        casnr.formula("LaF3").synonym("lanthanum fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13864-01-2" };
+        casnr.formula("LaH3").synonym("lanthanum hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13813-22-4" };
+        casnr.formula("LaI3").synonym("lanthanum iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25764-10-7" };
+        casnr.formula("LaN").synonym("lanthanum nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12031-17-3" };
+        casnr.formula("LaNbO4").synonym("lanthanum niobate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12056-90-5" };
+        casnr.formula("LaSi2").synonym("lanthanum silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13939-40-7" };
+        casnr.formula("LaVO4").synonym("lanthanum vanadate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1312-81-8" };
+        casnr.formula("La2O3").synonym("lanthanum oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12031-49-1" };
+        casnr.formula("La2S3").synonym("lanthanum sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16853-85-3" };
+        casnr.formula("LiAlH4").synonym("lithium tetrahydridoaluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12003-67-7" };
+        casnr.formula("LiAlO2").synonym("lithium aluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14283-07-9" };
+        casnr.formula("LiBF4").synonym("lithium tetrafluoroborate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16949-15-8" };
+        casnr.formula("LiBH4").synonym("lithium borohydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13453-69-5" };
+        casnr.formula("LiBO2").synonym("lithium metaborate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7550-35-8" };
+        casnr.formula("LiBr").synonym("lithium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "556-63-8" };
+        casnr.formula("LiCHO2").synonym("lithium formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "865-34-9" };
+        casnr.formula("LiCH3O").synonym("lithium methoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "553-54-8" };
+        casnr.formula("LiC7H5O2").synonym("lithium benzoate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7447-41-8" };
+        casnr.formula("LiCl").synonym("lithium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16712-20-2" };
+        casnr.formula("LiCl•H2O").synonym("lithium chloride monohydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7791-03-9" };
+        casnr.formula("LiClO4").synonym("lithium perchlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-24-4" };
+        casnr.formula("LiF").synonym("lithium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7580-67-8" };
+        casnr.formula("LiH").synonym("lithium hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10377-51-2" };
+        casnr.formula("LiI").synonym("lithium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-22-9" };
+        casnr.formula("LiI•3H2O").synonym("lithium iodide trihydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "4111-54-0" };
+        casnr.formula("LiN(C3H7)2").synonym("lithium diisopropylamide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7782-89-0" };
+        casnr.formula("LiNH2").synonym("lithium amide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-69-4" };
+        casnr.formula("LiNO3").synonym("lithium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "19597-69-4" };
+        casnr.formula("LiN3").synonym("lithium azide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12031-63-9" };
+        casnr.formula("LiNbO3").synonym("lithium niobate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1310-65-2" };
+        casnr.formula("LiOH").synonym("lithium hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12136-56-0" };
+        casnr.formula("LiO2").synonym("lithium superoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "21324-40-3" };
+        casnr.formula("LiPF6").synonym("lithium hexafluorophosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13453-80-0" };
+        casnr.formula("LiPH2O4").synonym("lithium dihydrogenphosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13768-48-4" };
+        casnr.formula("LiReO4").synonym("lithium perrhenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12031-66-2" };
+        casnr.formula("LiTaO3").synonym("lithium tantalate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15060-59-0" };
+        casnr.formula("LiVO3").synonym("lithium metavanadate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12007-60-2" };
+        casnr.formula("Li2B4O7").synonym("lithium borate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "554-13-2" };
+        casnr.formula("Li2CO3").synonym("lithium carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13568-40-6" };
+        casnr.formula("Li2MoO4").synonym("lithium molybdate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12057-24-8" };
+        casnr.formula("Li2O").synonym("lithium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12031-80-0" };
+        casnr.formula("Li2O2").synonym("lithium peroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12136-58-2" };
+        casnr.formula("Li2S").synonym("lithium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10377-48-7" };
+        casnr.formula("Li2SO4").synonym("lithium sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12136-60-6" };
+        casnr.formula("Li2Se").synonym("lithium selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10102-24-6" };
+        casnr.formula("Li2SiO3").synonym("lithium metasilicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12136-59-3" };
+        casnr.formula("Li2Te").synonym("lithium telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12031-82-2" };
+        casnr.formula("Li2TiO3").synonym("lithium titanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "51222-70-9" };
+        casnr.formula("Li2ZrO3").synonym("lithium zirconate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13821-20-0" };
+        casnr.formula("Li3AlF6").synonym("lithium hexafluoroaluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13478-14-3" };
+        casnr.formula("Li3AsO4").synonym("lithium arsenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "919-16-4" };
+        casnr.formula("Li3C6H5O7").synonym("lithium citrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7791-03-9" };
+        casnr.formula("Li3ClO4").synonym("lithium perchlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "26134-62-3" };
+        casnr.formula("Li3N").synonym("lithium nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10377-52-3" };
+        casnr.formula("Li3PO4").synonym("lithium phosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12057-30-6" };
+        casnr.formula("Li3Sb").synonym("lithium antimonide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12688-52-7" };
+        casnr.formula("LuB4").synonym("lutetium boride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14456-53-2" };
+        casnr.formula("LuBr3").synonym("lutetium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10099-66-8" };
+        casnr.formula("LuCl3").synonym("lutetium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13760-81-1" };
+        casnr.formula("LuF3").synonym("lutetium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13813-45-1" };
+        casnr.formula("LuI3").synonym("lutetium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12125-25-6" };
+        casnr.formula("LuN").synonym("lutetium nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "36549-50-5" };
+        casnr.formula("Lu(NO3)3").synonym("lutetium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12032-20-1" };
+        casnr.formula("Lu2O3").synonym("lutetium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12163-20-1" };
+        casnr.formula("Lu2S3").synonym("lutetium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12163-22-3" };
+        casnr.formula("Lu2Te3").synonym("lutetium telluride");
+        casnr.check();
+    }
+}
---
+++ src/check_L.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_L_H
+#error Already included
+#else
+#define CHECK_L_H
+
+extern void check_L();
+
+#endif
