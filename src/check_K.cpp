2faacf5c3563ba44236b6f952dfc2c3bed8da012
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-03 21:32:43 +0100

    Added files

---
+++ src/check_K.cpp
@@ -0,0 +1,376 @@
+#include "check_K.h"
+#include "cas_number.h"
+
+void check_K()
+{
+    {
+        cas_number casnr { "14484-69-6" };
+        casnr.formula("KAlF4").synonym("potassium tetrafluoroaluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12003-63-3" };
+        casnr.formula("KAlO2").synonym("potassium aluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14075-53-7" };
+        casnr.formula("KBF4").synonym("potassium fluoroborate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7758-02-3" };
+        casnr.formula("KBr").synonym("potassium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7758-01-2" };
+        casnr.formula("KBrO3").synonym("potassium bromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "2926-27-4" };
+        casnr.formula("KCHF3O3S").synonym("potassium trifluoromethanesulfonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "590-29-4" };
+        casnr.formula("KCHO2").synonym("potassium formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "865-33-8" };
+        casnr.formula("KCH3O").synonym("potassium methoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "151-50-8" };
+        casnr.formula("KCN").synonym("potassium cyanide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "590-28-3" };
+        casnr.formula("KCNO").synonym("potassium cyanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "127-95-7" };
+        casnr.formula("KC2HO4").synonym("potassium binoxalate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "127-08-2" };
+        casnr.formula("KC2H3O2").synonym("potassium acetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "917-58-8" };
+        casnr.formula("KC2H5O").synonym("potassium ethoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "327-62-8" };
+        casnr.formula("KC3H5O2").synonym("potassium propionate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "140-89-6" };
+        casnr.formula("KC3H5S2O").synonym("potassium xanthogenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "128-03-0" };
+        casnr.formula("KC3H7NS2").synonym("potassium dimethyldithiocarbamate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "868-14-4" };
+        casnr.formula("KC4H5O6").synonym("potassium bitartrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "582-25-2" };
+        casnr.formula("KC7H5O2").synonym("potassium benzoate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7447-40-7" };
+        casnr.formula("KCl").synonym("potassium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "3811-04-9" };
+        casnr.formula("KClO3").synonym("potassium chlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7778-74-7" };
+        casnr.formula("KClO4").synonym("potassium perchlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13877-24-2" };
+        casnr.formula("K2CuCl4").synonym("potassium tetrachlorocuprate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-23-3" };
+        casnr.formula("KF").synonym("potassium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7693-26-7" };
+        casnr.formula("KH").synonym("potassium hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "298-14-6" };
+        casnr.formula("KHCO3").synonym("potassium bicarbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-29-9" };
+        casnr.formula("K(HF2)").synonym("potassium bifluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7646-93-7" };
+        casnr.formula("KHSO4").synonym("potassium bisulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7778-77-0" };
+        casnr.formula("KH2PO4").synonym("potassium diphosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13762-51-1" };
+        casnr.formula("KH4B").synonym("potassium borohydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7681-11-0" };
+        casnr.formula("KI").synonym("potassium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7758-05-6" };
+        casnr.formula("KIO3").synonym("potassium iodate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-21-8" };
+        casnr.formula("KIO4").synonym("potassium periodate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7722-64-7" };
+        casnr.formula("KMnO4").synonym("potassium permanganate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7758-09-0" };
+        casnr.formula("KNO2").synonym("potassium nitrite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7757-79-1" };
+        casnr.formula("KNO3").synonym("potassium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20762-60-1" };
+        casnr.formula("KN3").synonym("potassium azide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12030-85-2" };
+        casnr.formula("KNbO3").synonym("potassium niobate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1310-58-3" };
+        casnr.formula("KOH").synonym("potassium hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12030-88-5" };
+        casnr.formula("KO2").synonym("potassium superoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "17084-13-8" };
+        casnr.formula("KPF6").synonym("potassium hexafluorophosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-53-6" };
+        casnr.formula("KPO3").synonym("potassium metaphosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10466-65-6" };
+        casnr.formula("KReO4").synonym("potassium perrhenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10378-50-4" };
+        casnr.formula("KRuO4").synonym("potassium perruthenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "333-20-0" };
+        casnr.formula("KSCN").synonym("potassium thiocyanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13769-43-2" };
+        casnr.formula("KVO3").synonym("potassium metavanadate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "584-08-7" };
+        casnr.formula("K2CO3").synonym("potassium carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "583-52-8" };
+        casnr.formula("K2C2O4").synonym("potassium oxalate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "921-53-9" };
+        casnr.formula("K2C4H6O6").synonym("potassium tartrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "19147-16-1" };
+        casnr.formula("K2C6H8O4").synonym("potassium adipate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-00-6" };
+        casnr.formula("K2CrO4").synonym("potassium chromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7778-50-9" };
+        casnr.formula("K2Cr2O7").synonym("potassium dichromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "39469-86-8" };
+        casnr.formula("K2FeO4").synonym("potassium ferrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-66-3" };
+        casnr.formula("K2S2O3").synonym("potassium thiosulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12136-45-7" };
+        casnr.formula("K2O").synonym("potassium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "17014-71-0" };
+        casnr.formula("K2O2").synonym("potassium peroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-98-6" };
+        casnr.formula("K2PdCl4").synonym("potassium tetrachloropalladate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18746-63-9" };
+        casnr.formula("K2RuCl6").synonym("potassium hexachlororuthenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1312-73-8" };
+        casnr.formula("K2S").synonym("potassium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7778-80-5" };
+        casnr.formula("K2SO4").synonym("potassium sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16731-55-8" };
+        casnr.formula("K2S2O5").synonym("potassium metabisulfite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-62-7" };
+        casnr.formula("K2S2O7").synonym("potassium pyrosulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7727-21-1" };
+        casnr.formula("K2S2O8").synonym("potassium persulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1312-74-9" };
+        casnr.formula("K2Se").synonym("potassium selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16871-90-2" };
+        casnr.formula("K2SiF6").synonym("potassium hexafluorosilicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10006-28-7" };
+        casnr.formula("K2SiO4").synonym("potassium silicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12142-40-4" };
+        casnr.formula("K2Te").synonym("potassium telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15571-91-2" };
+        casnr.formula("K2TeO3").synonym("potassium tellurite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16919-27-0" };
+        casnr.formula("K2TiF6").synonym("potassium hexafluorotitanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16923-95-8" };
+        casnr.formula("K2ZrF6").synonym("potassium hexafluorozirconate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13775-52-5" };
+        casnr.formula("K3AlF6").synonym("potassium hexafluoroaluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13746-66-2" };
+        casnr.formula("K3(Fe(CN)6)").synonym("potassium ferricyanide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7778-53-2" };
+        casnr.formula("K3PO4").synonym("potassium phosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7320-34-5" };
+        casnr.formula("K4P2O7").synonym("potassium pyrophosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13773-81-4" };
+        casnr.formula("KrF2").synonym("krypton difluoride");
+        casnr.check();
+    }
+}
---
+++ src/check_K.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_K_H
+#error Already included
+#else
+#define CHECK_K_H
+
+extern void check_K();
+
+#endif
