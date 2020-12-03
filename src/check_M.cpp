3bcf7a4b087814c7dadafddecee5af1fcf255f8c
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-03 21:43:03 +0100

    Added files

---
+++ src/check_M.cpp
@@ -0,0 +1,396 @@
+#include "check_M.h"
+#include "cas_number.h"
+
+void check_M()
+{
+    {
+        cas_number casnr { "7789-48-2" };
+        casnr.formula("MgBr2").synonym("magnesium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-53-2" };
+        casnr.formula("MgBr2•6H2O").synonym("magnesium bromide hexahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "546-93-0" };
+        casnr.formula("MgCO3").synonym("magnesium carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "556-32-1" };
+        casnr.formula("Mg(C2H2O2)2").synonym("magnesium succinate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "927-20-8" };
+        casnr.formula("MgC3H9O6P").synonym("magnesium glycerophosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "557-04-0" };
+        casnr.formula("MgC36H70O4").synonym("magnesium stearate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10034-81-8" };
+        casnr.formula("Mg(ClO4)2").synonym("magnesium perchlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7786-30-3" };
+        casnr.formula("MgCl2").synonym("magnesium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-40-6" };
+        casnr.formula("MgF2").synonym("magnesium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7693-27-8" };
+        casnr.formula("MgH2").synonym("magnesium hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10377-58-9" };
+        casnr.formula("MgI2").synonym("magnesium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10377-60-3" };
+        casnr.formula("Mg(NO3)2").synonym("magnesium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1309-48-4" };
+        casnr.formula("MgO").synonym("magnesium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1309-42-8" };
+        casnr.formula("Mg(OH)2").synonym("magnesium hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1335-26-8" };
+        casnr.formula("MgO2").synonym("magnesium peroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12032-36-9" };
+        casnr.formula("MgS").synonym("magnesium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7487-88-9" };
+        casnr.formula("MgSO4").synonym("magnesium sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1313-04-8" };
+        casnr.formula("MgSe").synonym("magnesium selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14654-06-9" };
+        casnr.formula("MgSiO3").synonym("magnesium silicite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "71683-38-0" };
+        casnr.formula("Mg(TcO4)2").synonym("magnesium technetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12032-44-9" };
+        casnr.formula("MgTe").synonym("magnesium telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12032-30-3" };
+        casnr.formula("MgTiO3").synonym("magnesium titanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "22831-39-6" };
+        casnr.formula("Mg2Si").synonym("magnesium silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10103-50-1" };
+        casnr.formula("Mg3(AsO4)2").synonym("magnesium arsenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12057-71-5" };
+        casnr.formula("Mg3N2").synonym("magnesium nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12057-75-9" };
+        casnr.formula("Mg3Sb2").synonym("magnesium antimonide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-03-2" };
+        casnr.formula("MnBr2").synonym("manganese(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "638-38-0" };
+        casnr.formula("Mn(C2H3O2)2").synonym("manganese(II) acetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "598-62-9" };
+        casnr.formula("MnCO3").synonym("manganese(II) carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7773-01-5" };
+        casnr.formula("MnCl2").synonym("manganese(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-34-9" };
+        casnr.formula("MnCl2•4H2O").synonym("manganese(II) chloride tetrahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14690-66-5" };
+        casnr.formula("MnCl3").synonym("manganese(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7782-64-1" };
+        casnr.formula("MnF2").synonym("manganese(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-53-1" };
+        casnr.formula("MnF3").synonym("manganese(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15195-58-1" };
+        casnr.formula("MnF4").synonym("manganese(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-33-2" };
+        casnr.formula("MnI2").synonym("manganese(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "98716-23-5" };
+        casnr.formula("MnI2•4H2O").synonym("manganese(II) iodide tetrahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10377-66-9" };
+        casnr.formula("Mn(NO3)2").synonym("manganese(II) nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1344-43-0" };
+        casnr.formula("MnO").synonym("manganese(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1313-13-9" };
+        casnr.formula("MnO2").synonym("manganese(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18820-29-6" };
+        casnr.formula("MnS").synonym("manganese(II) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10034-96-5" };
+        casnr.formula("MnSO4").synonym("manganese(II) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12032-82-5" };
+        casnr.formula("MnSb").synonym("manganese(III) antimonide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1313-22-0" };
+        casnr.formula("MnSe").synonym("manganese(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12299-98-8" };
+        casnr.formula("MnSe2").synonym("manganese(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12032-88-1" };
+        casnr.formula("MnTe").synonym("manganese(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12032-89-2" };
+        casnr.formula("MnTe2").synonym("manganese(IV) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1317-34-6" };
+        casnr.formula("Mn2O3").synonym("manganese(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12057-92-0" };
+        casnr.formula("Mn2O7").synonym("manganese(VII) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1317-35-7" };
+        casnr.formula("Mn3O4").synonym("manganese(II,III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14154-09-7" };
+        casnr.formula("Mn3(PO4)2").synonym("manganese(II) phosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-56-5" };
+        casnr.formula("MoBr2").synonym("molybdenum(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-57-6" };
+        casnr.formula("MoBr3").synonym("molybdenum(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13520-59-7" };
+        casnr.formula("MoBr4").synonym("molybdenum(IV) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13939-06-5" };
+        casnr.formula("MoC6O6").synonym("molybdenum(III) hexacarbonyl");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13478-17-6" };
+        casnr.formula("MoCl2").synonym("molybdenum(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13478-18-7" };
+        casnr.formula("MoCl3").synonym("molybdenum(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13478-18-7" };
+        casnr.formula("MoCl4").synonym("molybdenum(IV) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10241-05-1" };
+        casnr.formula("MoCl5").synonym("molybdenum(V) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13706-19-9" };
+        casnr.formula("MoCl6").synonym("molybdenum(VI) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20193-58-2" };
+        casnr.formula("MoF3").synonym("molybdenum(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "23412-45-5" };
+        casnr.formula("MoF4").synonym("molybdenum(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13819-84-6" };
+        casnr.formula("MoF5").synonym("molybdenum(V) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-77-9" };
+        casnr.formula("MoF6").synonym("molybdenum(VI) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14055-74-4" };
+        casnr.formula("MoI2").synonym("molybdenum(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14055-75-5" };
+        casnr.formula("MoI3").synonym("molybdenum(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14055-76-6" };
+        casnr.formula("MoI4").synonym("molybdenum(IV) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12033-19-1" };
+        casnr.formula("MoN").synonym("molybdenum(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12058-07-0" };
+        casnr.formula("MoO").synonym("molybdenum(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18868-43-4" };
+        casnr.formula("MoO2").synonym("molybdenum(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1313-27-5" };
+        casnr.formula("MoO3").synonym("molybdenum(VI) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1317-33-5" };
+        casnr.formula("MoS2").synonym("molybdenum(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12058-18-3" };
+        casnr.formula("MoSe2").synonym("molybdenum(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12136-78-6" };
+        casnr.formula("MoSi2").synonym("molybdenum(IV) silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12058-20-7" };
+        casnr.formula("MoTe2").synonym("molybdenum(IV) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1313-29-7" };
+        casnr.formula("Mo2O3").synonym("molybdenum(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12163-73-4" };
+        casnr.formula("Mo2O5").synonym("molybdenum(V) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12033-33-9" };
+        casnr.formula("Mo2S3").synonym("molybdenum(III) sulfide");
+        casnr.check();
+    }
+}
---
+++ src/check_M.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_M_H
+#error Already included
+#else
+#define CHECK_M_H
+
+extern void check_M();
+
+#endif
