aadacaa0eca7cbe8239a64cc4f1321025ee45e92
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-05 20:13:46 +0100

    Added files

---
+++ src/check_R.cpp
@@ -0,0 +1,406 @@
+#include "check_R.h"
+#include "cas_number.h"
+
+void check_R()
+{
+    {
+        cas_number casnr { "10031-23-9" };
+        casnr.formula("RaBr2").synonym("radium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7116-98-5" };
+        casnr.formula("RaCO3").synonym("radium carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-66-8" };
+        casnr.formula("RaCl2").synonym("radium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20610-49-5" };
+        casnr.formula("RaF2").synonym("radium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20610-52-0" };
+        casnr.formula("RaI2").synonym("radium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10213-12-4" };
+        casnr.formula("RaNO3").synonym("radium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12143-02-1" };
+        casnr.formula("RaO").synonym("radium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "98966-86-0" };
+        casnr.formula("Ra(OH)2").synonym("radium hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-16-4" };
+        casnr.formula("RaSO4").synonym("radium sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "17992-02-8" };
+        casnr.formula("RbAlCl4").synonym("rubidium tetrachloroaluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14484-70-9" };
+        casnr.formula("RbAlF4").synonym("rubidium tetrafluoroaluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-39-1" };
+        casnr.formula("RbBr").synonym("rubidium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "563-67-7" };
+        casnr.formula("RbC2H3O2").synonym("rubidium acetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "66169-93-5" };
+        casnr.formula("RbC5H7O2").synonym("rubidium acetylacetonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7791-11-9" };
+        casnr.formula("RbCl").synonym("rubidium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-74-7" };
+        casnr.formula("RbF").synonym("rubidium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-75-8" };
+        casnr.formula("RbH").synonym("rubidium hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-29-6" };
+        casnr.formula("RbI").synonym("rubidium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13446-76-9" };
+        casnr.formula("RbIO3").synonym("rubidium iodate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13465-48-0" };
+        casnr.formula("RbIO4").synonym("rubidium periodate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13126-12-0" };
+        casnr.formula("RbNO3").synonym("rubidium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1310-82-3" };
+        casnr.formula("RbOH").synonym("rubidium hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12137-25-6" };
+        casnr.formula("RbO2").synonym("rubidium superoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18088-11-4" };
+        casnr.formula("Rb2O").synonym("rubidium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "23611-30-5" };
+        casnr.formula("Rb2O2").synonym("rubidium peroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "31083-74-6" };
+        casnr.formula("Rb2S").synonym("rubidium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "31052-43-4" };
+        casnr.formula("Rb2Se").synonym("rubidium selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12210-70-7" };
+        casnr.formula("Rb2Te").synonym("rubidium telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13569-49-8" };
+        casnr.formula("ReBr3").synonym("rhenium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "36753-03-4" };
+        casnr.formula("ReBr4").synonym("rhenium(IV) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "30937-53-2" };
+        casnr.formula("ReBr5").synonym("rhenium(V) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13569-63-6" };
+        casnr.formula("ReCl3").synonym("rhenium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13569-71-6" };
+        casnr.formula("ReCl4").synonym("rhenium(IV) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13596-35-5" };
+        casnr.formula("ReCl5").synonym("rhenium(V) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "31234-26-1" };
+        casnr.formula("ReCl6").synonym("rhenium(VI) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15192-42-4" };
+        casnr.formula("ReF4").synonym("rhenium(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "30937-52-1" };
+        casnr.formula("ReF5").synonym("rhenium(V) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10049-17-9" };
+        casnr.formula("ReF6").synonym("rhenium(VI) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "17029-21-9" };
+        casnr.formula("ReF7").synonym("rhenium(VII) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15622-42-1" };
+        casnr.formula("ReI3").synonym("rhenium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "59301-47-2" };
+        casnr.formula("ReI4").synonym("rhenium(IV) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-09-8" };
+        casnr.formula("ReO2").synonym("rhenium(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-28-9" };
+        casnr.formula("ReO3").synonym("rhenium(VI) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12038-63-0" };
+        casnr.formula("ReS2").synonym("rhenium(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12067-00-4" };
+        casnr.formula("ReTe2").synonym("rhenium(IV) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12060-05-8" };
+        casnr.formula("Re2O3").synonym("rhenium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-68-7" };
+        casnr.formula("Re2O7").synonym("rhenium(VII) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12038-67-4" };
+        casnr.formula("Re2S7").synonym("rhenium(VII) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15608-29-4" };
+        casnr.formula("RhBr3").synonym("rhodium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10049-07-7" };
+        casnr.formula("RhCl3").synonym("rhodium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "60804-25-3" };
+        casnr.formula("RhF3").synonym("rhodium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "60617-65-4" };
+        casnr.formula("RhF4").synonym("rhodium(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "41517-05-9" };
+        casnr.formula("RhF5").synonym("rhodium(V) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13693-07-7" };
+        casnr.formula("RhF6").synonym("rhodium(VI) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15492-38-3" };
+        casnr.formula("RhI3").synonym("rhodium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13465-43-5" };
+        casnr.formula("Rh(NO3)3").synonym("rhodium(III) nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12137-27-8" };
+        casnr.formula("RhO2").synonym("rhodium(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12038-73-2" };
+        casnr.formula("RhS2").synonym("rhodium(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12038-76-5" };
+        casnr.formula("RhSe2").synonym("rhodium(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12038-80-1" };
+        casnr.formula("RhTe2").synonym("rhodium(IV) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-35-0" };
+        casnr.formula("Rh2O3").synonym("rhodium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10489-46-0" };
+        casnr.formula("Rh2(SO4)3").synonym("rhodium(III) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12067-06-0" };
+        casnr.formula("Rh2S3").synonym("rhodium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12038-67-4" };
+        casnr.formula("Rh2S7").synonym("rhodium(VII) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "59201-36-4" };
+        casnr.formula("RuBr2").synonym("ruthenium(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14014-88-1" };
+        casnr.formula("RuBr3").synonym("ruthenium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "22594-69-0" };
+        casnr.formula("RuC15H21O3").synonym("ruthenium(III) acetylacetonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13465-51-5" };
+        casnr.formula("RuCl2").synonym("ruthenium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10049-08-8" };
+        casnr.formula("RuCl3").synonym("ruthenium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "51621-05-7" };
+        casnr.formula("RuF3").synonym("ruthenium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "71500-16-8" };
+        casnr.formula("RuF4").synonym("ruthenium(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14521-18-7" };
+        casnr.formula("RuF5").synonym("ruthenium(V) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13693-08-8" };
+        casnr.formula("RuF6").synonym("ruthenium(VI) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "59201-41-1" };
+        casnr.formula("RuI2").synonym("ruthenium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13896-65-6" };
+        casnr.formula("RuI3").synonym("ruthenium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-10-1" };
+        casnr.formula("RuO2").synonym("ruthenium(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20427-56-9" };
+        casnr.formula("RuO4").synonym("ruthenium(VI) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-20-0" };
+        casnr.formula("RuS2").synonym("ruthenium(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-21-1" };
+        casnr.formula("RuSe2").synonym("ruthenium(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-21-1" };
+        casnr.formula("RuTe2").synonym("ruthenium(IV) telluride");
+        casnr.check();
+    }
+}
---
+++ src/check_R.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_R_H
+#error Already included
+#else
+#define CHECK_R_H
+
+extern void check_R();
+
+#endif
