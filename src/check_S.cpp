eddfc3cfb0e475fc90bf264ce3df2111f960f586
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-05 20:23:39 +0100

    Added files

---
+++ src/check_S.cpp
@@ -0,0 +1,671 @@
+#include "check_S.h"
+#include "cas_number.h"
+
+void check_S()
+{
+    {
+        cas_number casnr { "10545-99-0" };
+        casnr.formula("SCl2").synonym("sulfur dichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13451-08-6" };
+        casnr.formula("SCl4").synonym("sulfur tetrachloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13814-25-0" };
+        casnr.formula("SF2").synonym("sulfur difluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-60-0" };
+        casnr.formula("SF4").synonym("sulfur tetrafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "2551-62-4" };
+        casnr.formula("SF6").synonym("sulfur hexafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13827-32-2" };
+        casnr.formula("SO").synonym("sulfur monoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-09-5" };
+        casnr.formula("SO2").synonym("sulfur dioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-11-9" };
+        casnr.formula("SO3").synonym("sulfur trioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13172-31-1" };
+        casnr.formula("S2Br2").synonym("disulfur dibromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-67-9" };
+        casnr.formula("S2Cl2").synonym("disulfur dichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10546-01-7" };
+        casnr.formula("S2F4").synonym("disulfur tetrafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "5714-22-7" };
+        casnr.formula("S2F10").synonym("disulfur decafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "53280-15-2" };
+        casnr.formula("S2I2").synonym("disulfur diiodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20901-21-7" };
+        casnr.formula("S2O").synonym("disulfur monoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "28950-34-7" };
+        casnr.formula("S4N4").synonym("tetrasulfur tetranitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-61-9" };
+        casnr.formula("SbBr3").synonym("antimony tribromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7791-16-4" };
+        casnr.formula("SbCl2F3").synonym("antimony dichlorotrifluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-91-9" };
+        casnr.formula("SbCl3").synonym("antimony trichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7647-18-9" };
+        casnr.formula("SbCl5").synonym("antimony pentachloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-56-4" };
+        casnr.formula("SbF3").synonym("antimony trifluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-70-2" };
+        casnr.formula("SbF5").synonym("antimony pentafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7803-52-3" };
+        casnr.formula("SbH3").synonym("antimony trihydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-44-5" };
+        casnr.formula("SbI3").synonym("antimony triiodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12333-57-2" };
+        casnr.formula("SbN").synonym("antimony trinitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "35600-19-2" };
+        casnr.formula("SbNbO4").synonym("antimony orthoniobate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1309-64-4" };
+        casnr.formula("Sb2O3").synonym("antimony trioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1332-81-6" };
+        casnr.formula("Sb2O4").synonym("antimony tetroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-60-9" };
+        casnr.formula("Sb2O5").synonym("antimony pentoxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-32-4" };
+        casnr.formula("Sb2(SO4)3").synonym("antimony trisulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1345-04-6" };
+        casnr.formula("Sb2S3").synonym("antimony trisulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-04-4" };
+        casnr.formula("Sb2S5").synonym("antimony pentasulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-05-5" };
+        casnr.formula("Sb2Se3").synonym("antimony triselenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1327-50-0" };
+        casnr.formula("Sb2Te3").synonym("antimony tritelluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13465-59-3" };
+        casnr.formula("ScBr3").synonym("scandium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "144026-79-9" };
+        casnr.formula("Sc(CF3SO3)3").synonym("scandium(III) trifluoromethanesulfonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "23405-01-8" };
+        casnr.formula("Sc(CHO2)3").synonym("scandium(III) formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10361-84-9" };
+        casnr.formula("ScCl3").synonym("scandium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13709-47-2" };
+        casnr.formula("ScF3").synonym("scandium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13598-30-6" };
+        casnr.formula("ScH2").synonym("scandium(II) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "43238-07-9" };
+        casnr.formula("ScH3").synonym("scandium(III) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14474-33-0" };
+        casnr.formula("ScI3").synonym("scandium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13465-60-6" };
+        casnr.formula("Sc(NO3)3").synonym("scandium(III) nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12060-08-1" };
+        casnr.formula("Sc2O3").synonym("scandium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-29-9" };
+        casnr.formula("Sc2S3").synonym("scandium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-44-8" };
+        casnr.formula("Sc2Te3").synonym("scandium(III) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "17655-37-7" };
+        casnr.formula("SeBr").synonym("selenium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "22987-45-7" };
+        casnr.formula("SeBr2").synonym("selenium dibromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-65-3" };
+        casnr.formula("SeBr4").synonym("selenium tetrabromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14457-70-6" };
+        casnr.formula("SeCl2").synonym("selenium dichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10026-03-6" };
+        casnr.formula("SeCl4").synonym("selenium tetrachloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "70421-43-1" };
+        casnr.formula("SeF2").synonym("selenium difluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13465-66-2" };
+        casnr.formula("SeF4").synonym("selenium tetrafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-79-1" };
+        casnr.formula("SeF6").synonym("selenium hexafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-51-7" };
+        casnr.formula("SeOBr2").synonym("selenium oxybromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7791-23-3" };
+        casnr.formula("SeOCl2").synonym("selenium oxychloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-08-4" };
+        casnr.formula("SeO2").synonym("selenium dioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14984-81-7" };
+        casnr.formula("SeO2F2").synonym("selenium dioxydifluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13768-86-0" };
+        casnr.formula("SeO3").synonym("selenium trioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-34-6" };
+        casnr.formula("SeS").synonym("selenium monosulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7488-56-4" };
+        casnr.formula("SeS2").synonym("selenium disulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "75926-26-0" };
+        casnr.formula("Se2S6").synonym("selenium hexasulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12007-81-7" };
+        casnr.formula("SiB4").synonym("silicon boride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-66-4" };
+        casnr.formula("SiBr4").synonym("silicon bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "409-21-2" };
+        casnr.formula("SiC").synonym("silicon carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10026-04-7" };
+        casnr.formula("SiCl4").synonym("silicon chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-61-1" };
+        casnr.formula("SiF4").synonym("silicon fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7803-62-5" };
+        casnr.formula("SiH4").synonym("silicon hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13465-84-4" };
+        casnr.formula("SiI4").synonym("silicon iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14808-60-7" };
+        casnr.formula("SiO2").synonym("silicon dioxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13759-10-9" };
+        casnr.formula("SiS2").synonym("silicon sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12033-89-5" };
+        casnr.formula("Si3N4").synonym("silicon nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "50801-97-3" };
+        casnr.formula("SmBr2").synonym("samarium(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13759-87-0" };
+        casnr.formula("SmBr3").synonym("samarium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "3252-54-8" };
+        casnr.formula("Sm(CHO2)3").synonym("samarium(III) formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13874-75-4" };
+        casnr.formula("SmCl2").synonym("samarium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10361-82-7" };
+        casnr.formula("SmCl3").synonym("samarium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15192-17-3" };
+        casnr.formula("SmF2").synonym("samarium(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13765-24-7" };
+        casnr.formula("SmF3").synonym("samarium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "32248-43-4" };
+        casnr.formula("SmI2").synonym("samarium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13813-25-7" };
+        casnr.formula("SmI3").synonym("samarium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12060-58-1" };
+        casnr.formula("Sm2O3").synonym("samarium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12067-22-0" };
+        casnr.formula("Sm2S3").synonym("samarium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-56-4" };
+        casnr.formula("Sm2Se3").synonym("samarium(III) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12040-00-5" };
+        casnr.formula("Sm2Te3").synonym("samarium(III) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13814-97-6" };
+        casnr.formula("Sn(BF4)2").synonym("tin(II) fluoroborate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10031-24-0" };
+        casnr.formula("SnBr2").synonym("tin(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-67-5" };
+        casnr.formula("SnBr4").synonym("tin(IV) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "814-94-8" };
+        casnr.formula("SnC2O4").synonym("tin(II) oxalate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7772-99-8" };
+        casnr.formula("SnCl2").synonym("tin(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7646-78-8" };
+        casnr.formula("SnCl4").synonym("tin(IV) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-47-3" };
+        casnr.formula("SnF2").synonym("tin(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-62-2" };
+        casnr.formula("SnF4").synonym("tin(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "2406-52-2" };
+        casnr.formula("SnH4").synonym("tin(IV) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10294-70-9" };
+        casnr.formula("SnI2").synonym("tin(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-47-8" };
+        casnr.formula("SnI4").synonym("tin(IV) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "21651-19-4" };
+        casnr.formula("SnO").synonym("tin(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18282-10-5" };
+        casnr.formula("SnO2").synonym("tin(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-95-0" };
+        casnr.formula("SnS").synonym("tin(II) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7488-55-3" };
+        casnr.formula("SnSO4").synonym("tin(II) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-01-1" };
+        casnr.formula("SnS2").synonym("tin(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-06-6" };
+        casnr.formula("SnSe").synonym("tin(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20770-09-6" };
+        casnr.formula("SnSe2").synonym("tin(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12040-02-7" };
+        casnr.formula("SnTe").synonym("tin(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12004-37-4" };
+        casnr.formula("SrAlO3").synonym("strontium aluminate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13703-84-9" };
+        casnr.formula("SrB2O4").synonym("strontium metaborate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12046-54-7" };
+        casnr.formula("SrB6").synonym("strontium boride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10476-81-0" };
+        casnr.formula("SrBr2").synonym("strontium bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-53-9" };
+        casnr.formula("SrBr2•6H2O").synonym("strontium bromide hexahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "543-94-2" };
+        casnr.formula("Sr(C2H3O2)2").synonym("strontium acetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1633-05-2" };
+        casnr.formula("SrCO3").synonym("strontium carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10476-85-4" };
+        casnr.formula("SrCl2").synonym("strontium chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7789-06-2" };
+        casnr.formula("SrCrO4").synonym("strontium chromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-48-4" };
+        casnr.formula("SrF2").synonym("strontium fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13598-33-9" };
+        casnr.formula("SrH2").synonym("strontium hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10476-86-5" };
+        casnr.formula("SrI2").synonym("strontium iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-04-7" };
+        casnr.formula("SrMoO4").synonym("strontium molybdate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-06-9" };
+        casnr.formula("Sr(NO2)2").synonym("strontium nitrite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10042-76-9" };
+        casnr.formula("Sr(NO3)2").synonym("strontium nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-11-0" };
+        casnr.formula("SrO").synonym("strontium oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18480-07-4" };
+        casnr.formula("Sr(OH)2").synonym("strontium hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-18-7" };
+        casnr.formula("SrO2").synonym("strontium peroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-96-1" };
+        casnr.formula("SrS").synonym("strontium sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "18807-10-8" };
+        casnr.formula("SrSCN").synonym("strontium thiocyanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13451-02-0" };
+        casnr.formula("SrSO3").synonym("strontium sulfite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13845-16-4" };
+        casnr.formula("SrS2O6").synonym("strontium dithionate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-07-7" };
+        casnr.formula("SrSe").synonym("strontium selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-21-1" };
+        casnr.formula("SrSeO4").synonym("strontium selenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12712-63-9" };
+        casnr.formula("SrSiO4").synonym("strontium silicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12143-34-9" };
+        casnr.formula("SrSnO3").synonym("strontium stannate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12060-59-2" };
+        casnr.formula("SrTiO3").synonym("strontium titanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13451-05-3" };
+        casnr.formula("SrWO4").synonym("strontium tungstate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-39-4" };
+        casnr.formula("SrZrO3").synonym("strontium zirconate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12504-16-4" };
+        casnr.formula("Sr3P2").synonym("strontium phosphide");
+        casnr.check();
+    }
+}
---
+++ src/check_S.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_S_H
+#error Already included
+#else
+#define CHECK_S_H
+
+extern void check_S();
+
+#endif
