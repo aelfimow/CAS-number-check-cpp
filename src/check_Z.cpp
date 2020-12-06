9b0e45661b054e237f58c7cf7ccb4eda6ce29d23
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-06 18:30:28 +0100

    Added files

---
+++ src/check_Z.cpp
@@ -0,0 +1,371 @@
+#include "check_Z.h"
+#include "cas_number.h"
+
+void check_Z()
+{
+    {
+        cas_number casnr { "10326-24-6" };
+        casnr.formula("Zn(AsO2)2").synonym("zinc arsenite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12006-40-5" };
+        casnr.formula("Zn(AsO3)2").synonym("zinc arsenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7699-45-8" };
+        casnr.formula("ZnBr2").synonym("zinc bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "557-21-1" };
+        casnr.formula("Zn(CN)2").synonym("zinc cyanide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "3486-35-9" };
+        casnr.formula("ZnCO3").synonym("zinc carbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "557-34-6" };
+        casnr.formula("Zn(C2H3O2)2").synonym("zinc acetate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "547-68-2" };
+        casnr.formula("ZnC2O4").synonym("zinc oxalate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1300-26-1" };
+        casnr.formula("ZnC3H7O6P").synonym("zinc glycerophosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "52723-61-2" };
+        casnr.formula("ZnC4H2O4").synonym("zinc fumarate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "6228-53-1" };
+        casnr.formula("ZnC4H4O4").synonym("zinc succinate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "557-28-8" };
+        casnr.formula("ZnC6H10O4").synonym("zinc propionate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16039-53-5" };
+        casnr.formula("ZnC6H10O6").synonym("zinc lactate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "137-30-4" };
+        casnr.formula("ZnC6H12N2S4").synonym("zinc dimethyldithiocarbonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "557-09-5" };
+        casnr.formula("Zn(C8H15O2)2").synonym("zinc caprylate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14324-55-1" };
+        casnr.formula("ZnC10H20N2S4").synonym("zinc dimethyldithiocarbamate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "557-05-1" };
+        casnr.formula("Zn(C18H35O2)2").synonym("zinc stearate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10361-95-2" };
+        casnr.formula("Zn(ClO3)2").synonym("zinc chlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10025-64-6" };
+        casnr.formula("Zn(ClO4)2").synonym("zinc perchlorate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7646-85-7" };
+        casnr.formula("ZnCl2").synonym("zinc chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13530-65-9" };
+        casnr.formula("ZnCrO4").synonym("zinc chromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12018-19-8" };
+        casnr.formula("ZnCr2O4").synonym("zinc chromite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14018-95-2" };
+        casnr.formula("ZnCr2O7").synonym("zinc dichromate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-49-5" };
+        casnr.formula("ZnF2").synonym("zinc fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14018-82-7" };
+        casnr.formula("ZnH2").synonym("zinc hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7790-37-6" };
+        casnr.formula("Zn(IO3)2").synonym("zinc iodate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10139-47-6" };
+        casnr.formula("ZnI2").synonym("zinc iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "23414-72-4" };
+        casnr.formula("Zn(MnO4)2").synonym("zinc permanganate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13767-32-3" };
+        casnr.formula("ZnMoO4").synonym("zinc molybdate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10102-02-0" };
+        casnr.formula("Zn(NO2)2").synonym("zinc nitrite");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7779-88-6" };
+        casnr.formula("Zn(NO3)2").synonym("zinc nitrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-13-2" };
+        casnr.formula("ZnO").synonym("zinc oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20427-58-1" };
+        casnr.formula("Zn(OH)2").synonym("zinc hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-22-3" };
+        casnr.formula("ZnO2").synonym("zinc peroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-98-3" };
+        casnr.formula("ZnS").synonym("zinc sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "557-42-6" };
+        casnr.formula("Zn(SCN)2").synonym("zinc thiocyanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7733-02-0" };
+        casnr.formula("ZnSO4").synonym("zinc sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-35-9" };
+        casnr.formula("ZnSb").synonym("zinc antimonide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-09-9" };
+        casnr.formula("ZnSe").synonym("zinc selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-37-2" };
+        casnr.formula("ZnSnO3").synonym("zinc stannate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-11-3" };
+        casnr.formula("ZnTe").synonym("zinc telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-43-0" };
+        casnr.formula("ZnTiO3").synonym("zinc titanate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13597-56-3" };
+        casnr.formula("ZnWO4").synonym("zinc tungstate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14883-46-6" };
+        casnr.formula("Zn2Fe(CN)6").synonym("zinc ferrocyanide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7446-26-6" };
+        casnr.formula("Zn2P2O7").synonym("zinc pyrophosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13597-65-4" };
+        casnr.formula("Zn2SiO4").synonym("zinc orthosilicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13464-44-3" };
+        casnr.formula("Zn3(AsO4)2").synonym("zinc arsenate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12880-01-2" };
+        casnr.formula("Zn3(BO3)2").synonym("zinc borate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1313-49-1" };
+        casnr.formula("Zn3N2").synonym("zinc nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-84-7" };
+        casnr.formula("Zn3P2").synonym("zinc phosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7779-90-0" };
+        casnr.formula("Zn3(PO4)2").synonym("zinc phosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "24621-18-9" };
+        casnr.formula("ZrBr3").synonym("zirconium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13777-25-8" };
+        casnr.formula("ZrBr4").synonym("zirconium(IV) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12070-14-3" };
+        casnr.formula("ZrC").synonym("zirconium(IV) carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13762-26-0" };
+        casnr.formula("ZrCl2").synonym("zirconium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10026-11-6" };
+        casnr.formula("ZrCl4").synonym("zirconium(IV) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-64-4" };
+        casnr.formula("ZrF4").synonym("zirconium(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7704-99-6" };
+        casnr.formula("ZrH2").synonym("zirconium(II) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15513-85-6" };
+        casnr.formula("ZrI2").synonym("zirconium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13779-87-8" };
+        casnr.formula("ZrI3").synonym("zirconium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13986-26-0" };
+        casnr.formula("ZrI4").synonym("zirconium(IV) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25658-42-8" };
+        casnr.formula("ZrN").synonym("zirconium(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14475-63-9" };
+        casnr.formula("Zr(OH)4").synonym("zirconium(IV) hydroxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-01-0" };
+        casnr.formula("ZrO").synonym("zirconium(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-23-4" };
+        casnr.formula("ZrO2").synonym("zirconium(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13765-95-2" };
+        casnr.formula("Zr(HPO4)").synonym("zirconium(IV) phosphate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12037-80-8" };
+        casnr.formula("ZrP2").synonym("zirconium(IV) phosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14644-61-2" };
+        casnr.formula("Zr(SO4)2").synonym("zirconium(IV) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-15-5" };
+        casnr.formula("ZrS2").synonym("zirconium(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-47-1" };
+        casnr.formula("ZrSe2").synonym("zirconium(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-90-6" };
+        casnr.formula("ZrSi2").synonym("zirconium(IV) silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10101-52-7" };
+        casnr.formula("ZrSiO4").synonym("zirconium(IV) silicate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "32321-65-6" };
+        casnr.formula("ZrTe2").synonym("zirconium(IV) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "16853-74-0" };
+        casnr.formula("Zr(WO4)2").synonym("zirconium(IV) tungstate");
+        casnr.check();
+    }
+}
---
+++ src/check_Z.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_Z_H
+#error Already included
+#else
+#define CHECK_Z_H
+
+extern void check_Z();
+
+#endif
