be92cf4cd42e1577dacfa7d1aec2b59b543cc09c
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-06 18:03:48 +0100

    Added files

---
+++ src/check_V.cpp
@@ -0,0 +1,156 @@
+#include "check_V.h"
+#include "cas_number.h"
+
+void check_V()
+{
+    {
+        cas_number casnr { "14890-41-6" };
+        casnr.formula("VBr2").synonym("vanadium(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-26-3" };
+        casnr.formula("VBr3").synonym("vanadium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13595-30-7" };
+        casnr.formula("VBr4").synonym("vanadium(IV) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12070-10-9" };
+        casnr.formula("VC").synonym("vanadium(IV) carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10580-52-6" };
+        casnr.formula("VCl2").synonym("vanadium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7718-98-1" };
+        casnr.formula("VCl3").synonym("vanadium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7632-51-1" };
+        casnr.formula("VCl4").synonym("vanadium(IV) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13842-80-3" };
+        casnr.formula("VF2").synonym("vanadium(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10049-12-4" };
+        casnr.formula("VF3").synonym("vanadium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10049-16-8" };
+        casnr.formula("VF4").synonym("vanadium(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-72-4" };
+        casnr.formula("VF5").synonym("vanadium(V) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13966-93-3" };
+        casnr.formula("VH").synonym("vanadium(I) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15513-84-5" };
+        casnr.formula("VI2").synonym("vanadium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15513-94-7" };
+        casnr.formula("VI3").synonym("vanadium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15831-18-2" };
+        casnr.formula("VI4").synonym("vanadium(IV) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "24646-85-3" };
+        casnr.formula("VN").synonym("vanadium(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12035-98-2" };
+        casnr.formula("VO").synonym("vanadium(II) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7727-18-6" };
+        casnr.formula("VOCl3").synonym("vanadium(V) oxytrichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13709-31-4" };
+        casnr.formula("VOF3").synonym("vanadium(V) oxytrifluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-21-4" };
+        casnr.formula("VO2").synonym("vanadium(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "27774-13-6" };
+        casnr.formula("VOSO4").synonym("vanadyl sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-28-8" };
+        casnr.formula("VS2").synonym("vanadium(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12299-51-3" };
+        casnr.formula("VSe2").synonym("vanadium(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-87-1" };
+        casnr.formula("VSi2").synonym("vanadium(IV) silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "35515-91-4" };
+        casnr.formula("VTe2").synonym("vanadium(IV) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-34-7" };
+        casnr.formula("V2O3").synonym("vanadium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-62-1" };
+        casnr.formula("V2O5").synonym("vanadium(V) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13701-70-7" };
+        casnr.formula("V2(SO4)3").synonym("vanadium(III) sulfate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1315-03-3" };
+        casnr.formula("V2S3").synonym("vanadium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-76-8" };
+        casnr.formula("V3Si").synonym("vanadium(II) silicide");
+        casnr.check();
+    }
+}
---
+++ src/check_V.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_V_H
+#error Already included
+#else
+#define CHECK_V_H
+
+extern void check_V();
+
+#endif
