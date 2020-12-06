aa2bec111a893e5b87a9c82059c2f873b4487caa
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-06 18:22:30 +0100

    Added files

---
+++ src/check_Y.cpp
@@ -0,0 +1,166 @@
+#include "check_Y.h"
+#include "cas_number.h"
+
+void check_Y()
+{
+    {
+        cas_number casnr { "12255-48-0" };
+        casnr.formula("YAs").synonym("yttrium(III) arsenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12008-32-1" };
+        casnr.formula("YB6").synonym("yttrium(III) boride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13469-98-2" };
+        casnr.formula("YBr3").synonym("yttrium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15331-71-2" };
+        casnr.formula("Y(CHO2)3").synonym("yttrium(III) formate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12071-35-1" };
+        casnr.formula("YC2").synonym("yttrium(II) carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "52093-30-8" };
+        casnr.formula("Y(CF3SO3)3").synonym("yttrium(III) trifluoromethanesulfonate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10361-92-9" };
+        casnr.formula("YCl3").synonym("yttrium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13709-49-4" };
+        casnr.formula("YF3").synonym("yttrium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13598-35-1" };
+        casnr.formula("YH2").synonym("yttrium(II) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13598-57-7" };
+        casnr.formula("YH3").synonym("yttrium(III) hydride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-38-7" };
+        casnr.formula("YI3").synonym("yttrium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25764-13-0" };
+        casnr.formula("YN").synonym("yttrium(III) nitride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12294-01-8" };
+        casnr.formula("YP").synonym("yttrium(III) phosphide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12186-97-9" };
+        casnr.formula("YSb").synonym("yttrium(III) antimonide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13566-12-6" };
+        casnr.formula("YVO4").synonym("yttrium(III) vanadate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-36-9" };
+        casnr.formula("Y2O3").synonym("yttrium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-19-9" };
+        casnr.formula("Y2S3").synonym("yttrium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "25502-05-0" };
+        casnr.formula("YbBr2").synonym("ytterbium(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13759-89-2" };
+        casnr.formula("YbBr3").synonym("ytterbium(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13874-77-6" };
+        casnr.formula("YbCl2").synonym("ytterbium(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "10361-91-8" };
+        casnr.formula("YbCl3").synonym("ytterbium(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "19423-87-1" };
+        casnr.formula("YbCl3•6H2O").synonym("ytterbium(III) chloride hexahydrate");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15192-18-4" };
+        casnr.formula("YbF2").synonym("ytterbium(II) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13760-80-0" };
+        casnr.formula("YbF3").synonym("ytterbium(III) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "19357-86-9" };
+        casnr.formula("YbI2").synonym("ytterbium(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13813-44-0" };
+        casnr.formula("YbI3").synonym("ytterbium(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-54-2" };
+        casnr.formula("YbSe").synonym("ytterbium(II) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-89-3" };
+        casnr.formula("YbSi5").synonym("ytterbium(V) silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12125-58-5" };
+        casnr.formula("YbTe").synonym("ytterbium(II) telluride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-37-0" };
+        casnr.formula("Yb2O3").synonym("ytterbium(III) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-20-2" };
+        casnr.formula("Yb2S3").synonym("ytterbium(III) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12166-52-8" };
+        casnr.formula("Yb2Se3").synonym("ytterbium(III) selenide");
+        casnr.check();
+    }
+}
---
+++ src/check_Y.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_Y_H
+#error Already included
+#else
+#define CHECK_Y_H
+
+extern void check_Y();
+
+#endif
