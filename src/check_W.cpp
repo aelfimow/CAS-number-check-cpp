eea696ed4ce0a7062da4625998d579e5bf6ad462
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-06 18:11:40 +0100

    Added files

---
+++ src/check_W.cpp
@@ -0,0 +1,166 @@
+#include "check_W.h"
+#include "cas_number.h"
+
+void check_W()
+{
+    {
+        cas_number casnr { "13470-10-5" };
+        casnr.formula("WBr2").synonym("tungsten(II) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15163-24-3" };
+        casnr.formula("WBr3").synonym("tungsten(III) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14055-81-3" };
+        casnr.formula("WBr4").synonym("tungsten(IV) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-11-6" };
+        casnr.formula("WBr5").synonym("tungsten(V) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13701-86-5" };
+        casnr.formula("WBr6").synonym("tungsten(VI) bromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12070-12-1" };
+        casnr.formula("WC").synonym("tungsten(IV) carbide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-12-7" };
+        casnr.formula("WCl2").synonym("tungsten(II) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20193-56-0" };
+        casnr.formula("WCl3").synonym("tungsten(III) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-13-8" };
+        casnr.formula("WCl4").synonym("tungsten(IV) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-14-9" };
+        casnr.formula("WCl5").synonym("tungsten(V) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13283-01-7" };
+        casnr.formula("WCl6").synonym("tungsten(VI) chloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13766-47-7" };
+        casnr.formula("WF4").synonym("tungsten(IV) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "19357-83-6" };
+        casnr.formula("WF5").synonym("tungsten(V) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "7783-82-6" };
+        casnr.formula("WF6").synonym("tungsten(VI) fluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13470-17-2" };
+        casnr.formula("WI2").synonym("tungsten(II) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "15513-69-6" };
+        casnr.formula("WI3").synonym("tungsten(III) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14055-84-6" };
+        casnr.formula("WI4").synonym("tungsten(IV) iodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "20213-56-3" };
+        casnr.formula("WOBr3").synonym("tungsten(V) oxytribromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13520-77-9" };
+        casnr.formula("WOBr4").synonym("tungsten(VI) oxytetrabromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14249-98-0" };
+        casnr.formula("WOCl3").synonym("tungsten(V) oxytrichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13520-78-0" };
+        casnr.formula("WOCl4").synonym("tungsten(VI) oxytetrachloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13520-79-1" };
+        casnr.formula("WOF4").synonym("tungsten(VI) oxytetrafluoride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12036-22-5" };
+        casnr.formula("WO2").synonym("tungsten(IV) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13520-75-7" };
+        casnr.formula("WO2Br2").synonym("tungsten(VI) dioxydibromide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "13520-76-8" };
+        casnr.formula("WO2Cl2").synonym("tungsten(VI) dioxydichloride");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "14447-89-3" };
+        casnr.formula("WO2I2").synonym("tungsten(VI) dioxydiiodide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "1314-35-8" };
+        casnr.formula("WO3").synonym("tungsten(VI) oxide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12138-09-9" };
+        casnr.formula("WS2").synonym("tungsten(IV) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12125-19-8" };
+        casnr.formula("WS3").synonym("tungsten(VI) sulfide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12067-46-8" };
+        casnr.formula("WSe2").synonym("tungsten(IV) selenide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12039-88-2" };
+        casnr.formula("WSi2").synonym("tungsten(IV) silicide");
+        casnr.check();
+    }
+    {
+        cas_number casnr { "12067-76-4" };
+        casnr.formula("WTe2").synonym("tungsten(IV) telluride");
+        casnr.check();
+    }
+}
---
+++ src/check_W.h
@@ -0,0 +1,8 @@
+#ifdef CHECK_W_H
+#error Already included
+#else
+#define CHECK_W_H
+
+extern void check_W();
+
+#endif
